459b1f7259f3f78909a7319e68e0a28a3b64079e
JamesLyddon <jameslyddon@gmail.com>
2023-07-10 13:35:03 +0100

    feat: basic counter functionality

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import {useState} from 'react'
 
 export default function App() {
+  
+  const [count, setCount] = useState(0)
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.countContainer}>
+          <Text style={styles.count}>
+            {count}
+          </Text>
+        </View>
+        <TouchableOpacity 
+          style={styles.btn}
+          onPress={() => setCount(prev => prev + 1)}          
+        >
+          <Text style={styles.btntext}>
+            👃
+          </Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn: {
+    backgroundColor: 'cornflowerblue',
+    padding: 50,
+    borderRadius: 70,
+  },
+  btntext: {
+    color: '#fff'
+  },
+  countContainer: {
+    position: 'absolute',
+    top: '25%',
+  },
+  count: {
+    fontSize: 50
+  }
 });
